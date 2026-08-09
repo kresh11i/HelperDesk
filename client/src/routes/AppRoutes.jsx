ac56430de28983fbd3fedd238a473bdc6c1e321c
Kresh <srikresh11@gmail.com>
2026-08-10 00:56:57 +0530

    setup project foundation and frontend-backend integration

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import Tickets from "../pages/Tickets";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,26 @@
+//Dependencies
 import express from 'express';
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
 
 const app = express();
+const corsPolicy = {
 
+    origin: ["http://localhost:5173"],
+    credentials: true,
+
+};
+
+//middlewares
+app.use(cors(corsPolicy));
 app.use(express.json());
+app.use(morgan('dev'))
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
     res.json({
-        message: "HelpDesk API is running"
+        message: "HelpDesk API is running Client connected"
     });
 });
 
